72c15542d8eeb7eafeda9e363e6ae4bfb243d21b
Silvia Zambrana Cruz <silviazambrana3@gmail.com>
2019-10-30 12:03:33 +0100

    Pickup Creado
    Actualizado 30/10/2019
    
    He creado el pick up.

---
+++ Assets/Scripts/Pickup.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickup : MonoBehaviour
+{
+    public int points = 1;
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 rotation = new Vector3(15, 30, 45) * Time.deltaTime;
+        transform.Rotate(rotation);
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,10 +8,12 @@ public class PlayerController : MonoBehaviour
     public Rigidbody myRigidbody;
     public float speed;
 
+    int score;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        score = 0; 
     }
 
     // Update is called once per frame
@@ -27,5 +29,18 @@ public class PlayerController : MonoBehaviour
 
         Vector3 movement = new Vector3(movementHorizontal, 0f, movementVertical);
         myRigidbody.AddForce(movement * speed);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //Comporo el tag del objeto contra el que he colisionado; para desactivarlo solo en el caso de que sea un Pickup
+        if (other.gameObject.CompareTag("Pickup"))
+        {
+            //Si es Pickup, lo descativo
+            other.gameObject.SetActive(false);
+
+            score += other.gameObject.GetComponent<Pickup>().points;
+            Debug.Log("Mi puntación es: " + score);
+        }
     }
 }
