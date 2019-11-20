2ee370cbdf6c4c64b09190f3c8a1d5e4a761bf49
Silvia Zambrana <silviazambrana3@gmail.com>
2019-11-20 12:03:24 +0100

    Actualizacion
    Incorporacion de Spawn y Cronómetro.

---
+++ Assets/Scripts/Chronometer.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Chronometer : MonoBehaviour
+{
+    public Text time;
+    public Text downtime;
+    float down = 120;
+    float chronometer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        time.text = "hola";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        chronometer += Time.deltaTime;
+        time.text = "" + Mathf.Floor(chronometer);
+
+        down -= Time.deltaTime;
+        downtime.text = "" + Mathf.Floor(down);
+    }
+}
---
+++ Assets/Scripts/DeathZone.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathZone : MonoBehaviour
+{
+
+    public Transform Spawn;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player")) ;
+        {
+            other.gameObject.GetComponent<Rigidbody>().velocity = Vector3.zero;
+            other.gameObject.GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
+            other.gameObject.transform.position = Spawn.position;
+          
+        }
+    }
+
+    
+}
